64879b96e17299fa08e07adc496a57ceb40756c0
sumit vishvakarma <svishvakarma@bestpeers.com>
2023-06-30 15:51:27 +0530

    Devise and Devise Token Auth initializer files

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-    resources :books
-    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :books
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
